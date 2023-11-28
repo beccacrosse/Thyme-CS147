bae98227073b4c2eca8b0e22029d9582db2fa398
beccacrosse <109544455+beccacrosse@users.noreply.github.com>
2023-11-28 15:26:28 -0800

    added hi user text on home page

--- thyme/app/index.js
+++ thyme/app/index.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View,Image } from "react-native";
+import { useState, useEffect } from "react";
 import { Link } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
@@ -7,19 +8,39 @@ import { Styles } from "../assets/themes";
 import forestImage from "../assets/icons/forest.png";
 
 export default function App() {
+  const [user, setUser] = useState("Anna");
   return (
-    <LinearGradient
-      // Background Linear Gradient
-      style={Styles.container}
-      colors={["#C2D8A4", "rgba(194, 216, 164, 0.00)"]}
-      start={{ x: 0.6, y: 0.4 }}
-    >
-      <Image
-        style={{ width: 500, height: 500, resizeMode: "stretch" }}
-        source={forestImage}
-      />
-    </LinearGradient>
+    
+       <LinearGradient
+        // Background Linear Gradient
+        style={Styles.container}
+        colors={['#C2D8A4', 'rgba(194, 216, 164, 0.00)']}
+        start={{x:0.6, y:0.4}}
+        
+      >
+
+    <Text variant="displayLarge" style={styles.welcomeText}>
+      Hello, {user}
+    </Text>
+         <Image
+          style = {{ width: 500,
+          height: 500,resizeMode: 'stretch'}}
+          source={forestImage}
+          />
+
+     
+      </LinearGradient>   
+
   );
 }
+
+const styles = StyleSheet.create({
+  welcomeText :{
+    fontSize: 42,
+    color: '#263E20',
+    marginBottom: 150,
+  },
+
+})
 
 //fill: linear-gradient(180deg, #C2D8A4 0.77%, rgba(194, 216, 164, 0.00) 100%);
--- thyme/app/log/index.js
+++ thyme/app/log/index.js
@@ -24,6 +24,7 @@ export default function Log() {
 
 
 
+
 // export const log = () => {
 //   return (
 //     <div>log</div>
