0b0c13d890ea03c7efcf859ccbb9b536defa22f2
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2023-11-28 19:02:22 -0800

    Working Anna Gao profile

--- thyme/app/connections/index.js
+++ thyme/app/connections/index.js
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from "expo-linear-gradient";
 import { Styles } from "../../assets/themes";
-import AnnaGaoImage from "../../assets/images/AnnaGao.png";
+
 import { Images } from "../../assets/themes";
+import Profile from "../../components/shared/Profile"
 
 export default function Connections() {
   return (
@@ -14,13 +15,13 @@ export default function Connections() {
       colors={["#C2D8A4", "rgba(194, 216, 164, 0.00)"]}
       start={{ x: 0.6, y: 0.4 }}
     >
-      <Text variant="displayLarge" style={Styles.welcomeMessage}>
-        Anna Gao
+      <Text variant="displayLarge" style={Styles.connectionsPageTitle}>
+        Connections
       </Text>
 
-      <Image
-        source={AnnaGaoImage}
-        style={{ width: 400, height: 400, borderRadius: 400 / 2 }}
+      <Profile
+        profileImage = {Images.anna}
+        profileSize = {200}
       />
       <StatusBar style="auto" />
     </LinearGradient>
