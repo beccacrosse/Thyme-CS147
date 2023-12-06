d168c95995612863d2af1c321a9c238c9d2411d7
cyanjade <cyanjd@stanford.edu>
2023-12-06 14:34:34 -0800

    Modified Async Storage and Started Connecting it to medication checkin

--- thyme/components/logPage/useMedLog.js
+++ thyme/components/logPage/useMedLog.js
@@ -1,33 +1,48 @@
-
-import { ScrollView, Dimensions, Image, StyleSheet, Text, Pressable, View } from "react-native";
+import {
+  ScrollView,
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+} from "react-native";
 import { Images, Styles, Icons } from "../../assets/themes";
 import React, { useState } from "react";
-import { Feather } from '@expo/vector-icons';
-import { Avatar, Button, Card, IconButton, } from "react-native-paper";
-import MedicationCheckIn from '../simple/MedicationCheckIn';
-
+import { Feather } from "@expo/vector-icons";
+import { Avatar, Button, Card, IconButton } from "react-native-paper";
+import MedicationCheckIn from "../simple/MedicationCheckIn";
+import { getData, setData } from "../../asyncStorage.js";
 
 const { height: windowHeight, width: windowWidth } = Dimensions.get("window");
+
 export default function useMedLog(medication) {
-
-  const avatarSize = 60;
+  const avatarSize = 50;
   const title = medication.name;
-  const image = medication.image
+  const image = medication.image;
   const dose = medication.dose;
-  const medicationDescription="TBD"
-  const daysDone= 1
-  const pillNumber =1
-  const goButton = 
-  <Pressable style={styles.goButton}>
-    <Feather name="arrow-right" size={45} color={"black"} />
-  </Pressable>
-  
+  const medicationDescription = "TBD";
+  const daysDone = 1;
+  const pillNumber = 1;
 
+  const goButton = (
+    <Pressable style={styles.goButton} onPress={() => {}}>
+      <Feather name="arrow-right" size={45} color={"black"} />
+    </Pressable>
+  );
+
+  const LeftContent = (props) => (
+    <Image
+      style={[styles.icon, { width: avatarSize, height: avatarSize }]}
+      source={medication.image}
+    />
+  );
+
+  const RightContent = (props) => <Card.Actions>{goButton}</Card.Actions>;
 
   //const [modalVisible, setModalVisible] = useState(false);
 
-  
-  return(
+  return (
     // <View style={{ marginBottom: 10 }}>
     //     <MedicationCheckIn
     //       medicationIcon={image}
@@ -39,79 +54,69 @@ export default function useMedLog(medication) {
     //       date={"DATE TBD"}
     //       isVisible={modalVisible}
     //     />
+    // <Card
+    //   style={{
+    //     backgroundColor: "white",
+    //     marginBottom: 20,
+    //     marginTop: 10,
+    //   }}
+    // >
     <Card
-          style={{
-            backgroundColor: "white",
-            marginBottom: 20,
-            marginTop: 10,
-          }}
-        >
-           <View
-          style={{
-            width: "full",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Card.Content>
-            <Image style={[
-            styles.icon,
-            { width: avatarSize, height: avatarSize },
-          ]} source={image} />
-          </Card.Content>
-          <Card.Title  title={title}
-            subtitle={dose}/>
-          <Card.Actions>
-              {goButton}
-          </Card.Actions>
-        </View>
-        </Card>
-  )
+      style={{
+        backgroundColor: "white",
+        marginBottom: 10,
+        marginTop: 10,
+      }}
+    >
+      <Card.Title
+        title={title}
+        subtitle={dose}
+        left={LeftContent}
+        right={RightContent}
+      />
+    </Card>
+  );
 }
 
 const styles = {
-  logContainer :{
+  logContainer: {
     backgroundColor: Styles.completedColor,
     flexDirection: "row",
     justifyContent: "space-between",
     borderRadius: "10%",
-    width: windowWidth * .9,
-    height: windowHeight * .085,
-
+    width: windowWidth * 0.9,
+    height: windowHeight * 0.085,
   },
   imageContainer: {
     flex: 1,
     borderWidth: 2,
-    borderColor: "red"
+    borderColor: "red",
   },
-  pillImage : {
-    aspectRatio:1,
+  pillImage: {
+    aspectRatio: 1,
   },
   pillInfo: {
     flex: 4.25,
     margin: 5,
     borderWidth: 2,
-    borderColor: "red"
+    borderColor: "red",
   },
-  logTextPrimary : {
+  logTextPrimary: {
     color: "black",
     fontSize: 35,
   },
-  logTextSecondary : {
+  logTextSecondary: {
     color: "black",
-    fontSize: 15
+    fontSize: 15,
   },
   buttonContainer: {
-    flex: .75,
+    flex: 0.75,
     borderWidth: 2,
     marginRight: 0,
-    borderColor: "red"
+    borderColor: "red",
   },
   goButton: {
-    borderWidth: 1,
-    borderColor: "blue"
-  }
-}
-
+    // borderWidth: 1,
+    // borderColor: "blue",
+  },
+};
